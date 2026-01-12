45b4f362088fc99aa7ec78a71cdb6b0c4b23044d
theKJEngineer <kjanas.it@gmail.com>
2026-01-12 12:07:36 +0100

    feat: add PassengerCar class implementing Vehicle contract

--- src/com/carrental/model/vehicle/PassengerCar.java
+++ src/com/carrental/model/vehicle/PassengerCar.java
@@ -1,4 +1,62 @@
 package com.carrental.model.vehicle;
 
-public class PassengerCar {
+public class PassengerCar extends Vehicle {
+    private String fuelType;
+    private String transmission;
+    private int numberOfDoors;
+    private int numberOfSeats;
+    private double fuelConsumption;
+
+    public PassengerCar(String brand, String model, String licensePlate,
+                        int productionYear, double mileage, double dailyRate,
+                        String fuelType, String transmission, int numberOfDors,
+                        int numberOfSeats, double fuelConsumption) {
+        super(brand, model, licensePlate, productionYear, mileage, dailyRate);
+        this.fuelType = fuelType;
+        this.transmission = transmission;
+        this.numberOfDoors = numberOfDors;
+        this.numberOfSeats = numberOfSeats;
+        this.fuelConsumption = fuelConsumption;
+    }
+
+    public String getFuelType() {
+        return fuelType;
+    }
+
+    public String getTransmission() {
+        return transmission;
+    }
+
+    public int getNumberOfDoors() {
+        return numberOfDoors;
+    }
+
+    public int getNumberOfSeats() {
+        return numberOfSeats;
+    }
+
+    public double getFuelConsumption() {
+        return fuelConsumption;
+    }
+
+    @Override
+    public double getFuelEfficiency() {
+        return  this.fuelConsumption;
+    }
+
+    @Override
+    public String getRequiredLicenseCategory() {
+        return "B";
+    }
+
+    @Override
+    public int getServiceFrequency() {
+        return 15000;
+    }
+    @Override
+    public String toString() {
+        return
+                super.toString()
+                + " " + this.fuelType + " " + this.transmission + " " + this.numberOfDoors + " " + this.numberOfSeats;
+    }
 }
