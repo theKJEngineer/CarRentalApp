f5364616d626f5f4648a6e0292925dee2f7c736f
theKJEngineer <kjanas.it@gmail.com>
2026-01-14 13:05:06 +0100

    Add Employee class for staff management

--- src/com/carrental/model/Employee.java
+++ src/com/carrental/model/Employee.java
@@ -1,4 +1,45 @@
 package com.carrental.model;
 
+/*
+ Represents a staff member working at the rental agency.
+ Stores employment details such as position and unique ID,
+ used for authorizing rentals and managing customer service.
+ */
+
 public class Employee {
+    private String firstName;
+    private String lastName;
+    private String position;
+    private String employeeId;
+
+    public Employee(String firstName, String lastName, String position, String employeeId) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.position = position;
+        this.employeeId = employeeId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    @Override
+    public String toString() {
+        return "First name: " + this.firstName +
+                ", Last name: " + this.lastName +
+                ", Position: " + this.position +
+                ", Employee ID: " + this.employeeId;
+    }
 }
