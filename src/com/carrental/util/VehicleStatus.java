1f2d07bd1a46a09afb2bda0782e6170007b052a0
theKJEngineer <kjanas.it@gmail.com>
2026-01-24 09:33:30 +0100

    feat(util): implement VehicleStatus enum with state descriptions

--- src/com/carrental/util/VehicleStatus.java
+++ src/com/carrental/util/VehicleStatus.java
@@ -1,4 +1,18 @@
 package com.carrental.util;
 
-public class VehicleStatus {
+public enum VehicleStatus {
+    AVAILABLE("Ready for rent"),
+    RENTED("Currently rented"),
+    UNAVAILABLE("Out of service / Repair");
+
+    private final String description;
+
+    VehicleStatus(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return this.name() + " (" + this.description + ")";
+    }
 }
